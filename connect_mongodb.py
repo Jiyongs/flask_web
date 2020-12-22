7d429a889a5e9514d6197304758d8b8a3a1e5789
jiyongs <shzy232@naver.com>
2020-12-22 23:19:06 +0900

    commit_4 : connect to mongodb

---
+++ connect_mongodb.py
@@ -0,0 +1,45 @@
+from bson.objectid import ObjectId
+from pymongo import MongoClient
+
+def connect_mongodb(db, collection):
+    mongo_client = MongoClient("mongodb://localhost:27017/")
+    # print(mongo_client.list_database_names())
+    db_instance = mongo_client[db]
+    return db_instance[collection]
+
+def insert_one(conn, data_dict):
+    try:
+        conn.insert_one(data_dict)
+        return True
+    except Exception as e:
+        print("Failed Insert data : {}".format(e))
+        return False
+
+def find_boards(conn):
+    board_list = conn.find()
+    board_dict = dict()
+
+    for b in board_list:
+        post_id = str(b['_id'])
+        del b['_id']
+        board_dict[post_id] = b
+
+    return board_dict
+
+def update_boards(conn, post_id, data):
+    try:
+        query = {'_id':ObjectId(post_id)}, {'$set': data}
+        conn.update_one(*query)
+        return True
+    except Exception as e:
+        print("Failed update data : {}".format(e))
+        return False
+
+def del_boards(conn, post_id):
+    try:
+        del_query = {"_id":ObjectId(post_id)}
+        conn.delete_one(del_query)
+        return True
+    except Exception as e:
+        print("Failed delete data : {}".format(e))
+        return False
--- flask_template.py
+++ flask_template.py
@@ -1,24 +1,29 @@
 from flask import Flask, render_template, request
 from datetime import datetime
+import connect_mongodb as mongo
 
 # create flask instance
 app = Flask(__name__)
-# define board list
-boards = {
-    '1' : {'user':'jiyoung', 'write_date' : '2020-12-19', 'title' : 'today', 'contents' : "Today is saturday.\nAnd I watched a series called 'Friends' all day long. It was so fun."},
-    '2' : {'user':'youngji', 'write_date' : '2020-12-20', 'title' : 'wannabe', 'contents' : "I just want to be a rich jobless."}
-}
+
+# connect to mongodb
+mongo_conn = mongo.connect_mongodb(db="jiyongs", collection="board")
+
+# boards = {
+#     '1' : {'user':'jiyoung', 'write_date' : '2020-12-19', 'title' : 'today', 'contents' : "Today is saturday.\nAnd I watched a series called 'Friends' all day long. It was so fun."},
+#     '2' : {'user':'youngji', 'write_date' : '2020-12-20', 'title' : 'wannabe', 'contents' : "I just want to be a rich jobless."}
+# }
 
 # define route for pages
 @app.route("/")
 def template_boards():
     return render_template(
         'index.html',
-        board_dict=boards
+        board_dict=mongo.find_boards(mongo_conn)
     )
 @app.route("/boards/<string:post_id>")
 def template_posts(post_id):
     board_dict = dict()
+    boards = mongo.find_boards(mongo_conn)
     if post_id in boards:
         board_dict = boards[post_id]
     return render_template(
@@ -29,35 +34,37 @@ def template_posts(post_id):
 @app.route("/new/", methods=['POST'])
 def create_post():
     post = request.form
-    post_id = str(len(boards) + 1)
-    boards[post_id] = {'user': post['user'],
+    post_dict = {'user': post['user'],
                        'write_date': get_current_date(),
                        'title': post['title'],
                        'contents': post['contents']
-                       }
+                }
+    mongo.insert_one(mongo_conn, post_dict)
     return render_template(
         'index.html',
-        board_dict=boards
+        board_dict=mongo.find_boards(mongo_conn)
+
     )
 @app.route("/edit/", methods=['POST'])
 def edit_post():
     post = request.form
     post_id = post['post_id']
-    boards[post_id] = {'user': post['user'],
-                       'write_date': post['write_date'],
-                       'title': post['title'],
-                       'contents': post['contents']
-                       }
+    post_dict = {'user': post['user'],
+                 'write_date': post['write_date'],
+                 'title': post['title'],
+                 'contents': post['contents']
+                }
+    mongo.update_boards(mongo_conn, post_id, post_dict)
     return render_template(
         'index.html',
-        board_dict=boards
+        board_dict=mongo.find_boards(mongo_conn)
     )
 @app.route("/delete/<string:post_id>")
 def del_post(post_id):
-    del boards[post_id]
+    mongo.del_boards(mongo_conn, post_id)
     return render_template(
         'index.html',
-        board_dict=boards
+        board_dict=mongo.find_boards(mongo_conn)
     )
 
 # get current date
